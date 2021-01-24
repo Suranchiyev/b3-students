95a112ab50ef967cc043c68e0a5167c1d7604fc8
Ermek87 <ermak.murat@gmail.com>
2021-01-24 10:39:37 -0500

    commit

--- src/Oleksii.java
+++ src/Oleksii.java
@@ -1,6 +1,7 @@
 public class Oleksii {
     public static void main(String[] args) {
         System.out.println("20 pushups for everybody");
+        System.out.println("and get heart attack!))))");
     }
 
 }
