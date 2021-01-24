df14227536ee96501b44e844a32a7f15bdaa534f
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2021-01-24 11:29:11 -0500

    log in

---
+++ src/LogIn.java
@@ -0,0 +1,8 @@
+public class LogIn {
+    public static void main(String[] args) {
+        System.out.println("Log in");
+        System.out.println("Username: bek");
+        System.out.println("Password:");
+        System.out.println("Logged in");
+    }
+}
